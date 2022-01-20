f5500383afb5f0b596916077098f32e74cb60e04
Marcos Santos Santos <marcos012santos@gmail.com>
2022-01-20 13:37:11 -0300

    iniciar 10.1

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -13,30 +13,41 @@ export const GithubProvider = ({children}) => {
     }
     const [state, dispatch] = useReducer(githubReducer, initialState)
     
-    // get initial users (testing purposes)
-    const fetchUsers = async () => {
+    // get search results
+    const searchUsers = async (text) => {
         setLoading()
 
-        const response = await fetch(`${GITHUB_URL}/users`,
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,
         {
             headers:{
                 Authorization:`token ${GITHUB_TOKEN}` 
             } 
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
 
         dispatch({
            type:'GET_USERS',
-           payload:  data
+           payload:  items
         })
     } 
+
+    // clear users from state
+    const clearUsers = () => dispatch({
+        type: 'CLEAR_USERS'
+    })
+
     // set loading
     const setLoading = () => dispatch({type:'SET_LOADING'}) 
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
     }}>
         {children}
     </GithubContext.Provider>
